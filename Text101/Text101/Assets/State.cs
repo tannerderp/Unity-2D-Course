c852691f2a1216fc4eb220ba00c1915f4ff50f82
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-07-10 12:27:44 -0600

    creating an array

--- Text101/Text101/Assets/State.cs
+++ Text101/Text101/Assets/State.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
     }
-
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
 }
